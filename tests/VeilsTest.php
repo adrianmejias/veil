914c90808b8234cec373f4ccf2d8d5d636c167fd
Adrian Mejias <adrianmejias@gmail.com>
2022-01-01 00:41:48 -0600

    wip

--- tests/VeilsTest.php
+++ tests/VeilsTest.php
@@ -94,6 +94,81 @@ class VeilsTest extends TestCase
      * @test
      * @covers \AdrianMejias\Veil\Veil
      * @covers \AdrianMejias\Veil\VeilAbstract
+     */
+    public function it_can_call_method_with_one_arg()
+    {
+        (new Veil)->register()->add('Foo', FooVeil::class);
+
+        $expected = \Foo::bar(1);
+        $actual = FooVeil::getVeilInstance()->bar(1);
+
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * @test
+     * @covers \AdrianMejias\Veil\Veil
+     * @covers \AdrianMejias\Veil\VeilAbstract
+     */
+    public function it_can_call_method_with_two_args()
+    {
+        (new Veil)->register()->add('Foo', FooVeil::class);
+
+        $expected = \Foo::bar(1, 2);
+        $actual = FooVeil::getVeilInstance()->bar(1, 2);
+
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * @test
+     * @covers \AdrianMejias\Veil\Veil
+     * @covers \AdrianMejias\Veil\VeilAbstract
+     */
+    public function it_can_call_method_with_three_args()
+    {
+        (new Veil)->register()->add('Foo', FooVeil::class);
+
+        $expected = \Foo::bar(1, 2, 3);
+        $actual = FooVeil::getVeilInstance()->bar(1, 2, 3);
+
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * @test
+     * @covers \AdrianMejias\Veil\Veil
+     * @covers \AdrianMejias\Veil\VeilAbstract
+     */
+    public function it_can_call_method_with_four_args()
+    {
+        (new Veil)->register()->add('Foo', FooVeil::class);
+
+        $expected = \Foo::bar(1, 2, 3, 4);
+        $actual = FooVeil::getVeilInstance()->bar(1, 2, 3, 4);
+
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * @test
+     * @covers \AdrianMejias\Veil\Veil
+     * @covers \AdrianMejias\Veil\VeilAbstract
+     */
+    public function it_can_call_method_with_five_args()
+    {
+        (new Veil)->register()->add('Foo', FooVeil::class);
+
+        $expected = \Foo::bar(1, 2, 3, 4, 5);
+        $actual = FooVeil::getVeilInstance()->bar(1, 2, 3, 4, 5);
+
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * @test
+     * @covers \AdrianMejias\Veil\Veil
+     * @covers \AdrianMejias\Veil\VeilAbstract
      * @covers \AdrianMejias\Veil\Exceptions\NoInstanceMethodFoundException
      */
     public function it_can_throw_exception_for_instance_method_not_found()
